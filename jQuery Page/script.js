27ded5c05fa1f600427aaf77ec4c9c04104cdc44
Fenravn <fenravn@proton.me>
2025-04-15 13:44:47 +0100

    Added basic code for jQuery Page

---
+++ jQuery Page/script.js
@@ -0,0 +1,7 @@
+// jquery-page/script.js
+$(document).ready(function () {
+    $('#btn-click').on('click', function () {
+      $('#click-result').text("You clicked the button!");
+    });
+  });
+  
