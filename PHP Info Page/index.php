690c5912e1336e72ef7f7e38b0fb6b9b2dc45548
Fenravn <fenravn@proton.me>
2025-04-16 18:30:40 +0100

    Added code for the php page

---
+++ PHP Info Page/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>PHP Page</title>
+  <meta name="generator" content="PHP">
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <h1>Welcome to the PHP Page</h1>
+  <p>This page is powered by PHP and rendered on the server.</p>
+  <p>Today's date is: <?php echo date('l, F j, Y'); ?></p>
+  <a href="info.php">PHP Info</a>
+</body>
+</html>
